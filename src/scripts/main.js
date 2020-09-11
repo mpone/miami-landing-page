997c8834576004391b4dc453ab05d65d83456aaf
mpone <m.parphenjuk@gmail.com>
2020-09-11 23:01:52 +0300

    Version 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const consultationForm = document.querySelector('.consultation .form');
+const contactsForm = document.querySelector('.contacts .form');
+
+consultationForm.addEventListener('submit', event => {
+  event.preventDefault();
+  consultationForm.reset();
+});
+
+contactsForm.addEventListener('submit', event => {
+  event.preventDefault();
+  contactsForm.reset();
+});
